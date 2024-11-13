c54ca1c6d6f79d58627a88213f7c7678b0a6f2fa
Nuutti Kekkonen <nuutti.kekkonen@saunalahti.fi>
2024-11-13 17:17:44 +0200

    render_selector added

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, url_for
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/selctor", methods=["GET"])
+def render_selector():
+    return render_template("selector.html")
