12e28beaa79d81da2ec532449ca1f868320a15f6
lottapispa <lotta.pispa@helsinki.fi>
2024-11-13 16:51:03 +0200

    app.py  muokattu

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import render_template, request
 app = Flask(__name__)
 
 
-@app.route("/index", methods=["POST"])
+@app.route("/", methods=["POST"])
 def index():
     return render_template("index.html", name=request.form["name"])
