98d8d37a3285f7eda4940756e4721935201c7b36
lottapispa <101987621+lottapispa@users.noreply.github.com>
2024-11-13 16:41:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import render_template, request
 app = Flask(__name__)
 
 
-@app.route("/frontpage", methods=["POST"])
+@app.route("/index", methods=["POST"])
 def index():
     return render_template("frontpage.html", name=request.form["name"])
