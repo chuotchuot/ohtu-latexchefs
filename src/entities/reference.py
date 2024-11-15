88a9276b2c23a4ea798b216424f52f7209119a09
Juho Kavekari <juhokavekari@hotmail.com>
2024-11-15 18:48:02 +0200

    Add reference class

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,1 +1,8 @@
+class Reference:
+    def __init__(self, title, year, authors):
+        self.title = title
+        self.year = year
+        self.authors = authors
 
+    def __str__(self):
+        pass
