01faba113d8eb0d67e7d0c3333dc12686902467a
Juho Kavekari <juhokavekari@hotmail.com>
2024-11-15 18:50:56 +0200

    Add db functions for testing

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,10 +1,52 @@
 from config import db, app
 from sqlalchemy import text
 
-table_name = "references"
+table_name = "reference"
+
+def table_exists(name):
+    sql = text(
+        "SELECT EXISTS"
+        "   SELECT 1"
+        "   FROM information_schema.tables"
+        f"   WHERE table_name = '{name}'"
+        ")"
+    )
+    
+    print(f"Checking if table {name} exists")
+    print(sql)
+
+    result = db.session.execute(sql)
+    return result.fetchall()[0][0]
+    
 
 def setup_db():
-    pass
+    if table_exists(table_name):
+       print(f"Table {table_name} exist. Dropping existing table")
+       sql = text(f"DROP TABLE {table_name}")
+       db.session.execute(sql)
+       db.session.commit()
+    
+    print(f"Creating table {table_name}")
+    sql = (text(
+        f"CREATE TABLE {table_name} ("
+        "   id SERIAL PRIMARY KEY," 
+        "   title TEXT,"
+        "   author TEXT," 
+        "   year INT," 
+        "   publisher TEXT," 
+        "   editor TEXT,"
+        "   reference_type TEXT)"
+        ))
+    db.session.execute(sql)
+    db.session.commit()
+
+
+def reset_db():
+    print(f"Clearing contents from table {table_name}")
+    sql = text(f"DELETE FROM {table_name}")
+    db.session.execute(sql)
+    db.session.commit()
+
 
 if __name__ == "__main__":
     with app.app_context():
