dfd6c21ee145ffadaff9f818a27eeefc41e2f627
chuot <152192561+chuotchuot@users.noreply.github.com>
2024-11-18 16:34:02 +0200

    Fix imports

--- src/unit_tests/reference_test.py
+++ src/unit_tests/reference_test.py
@@ -1,12 +1,11 @@
 import unittest
-from entities.reference import Reference
+from src.entities.reference import Reference
 
-class TestReference(unittest.Testcase):
+class TestReference(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.correct_reference = Reference(
             "Test-driven Python development : develop high-quality and maintainable Python applications using the principles of test-driven development ", 
             2015, 
             "Siddharta Govindaraj"
-        
         )
