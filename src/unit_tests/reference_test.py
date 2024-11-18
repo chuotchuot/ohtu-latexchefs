290b2fa0214bc874b67c4852220805dd4f68866d
chuot <152192561+chuotchuot@users.noreply.github.com>
2024-11-18 16:59:37 +0200

    Add unit test for ref creation

--- src/unit_tests/reference_test.py
+++ src/unit_tests/reference_test.py
@@ -5,7 +5,12 @@ class TestReference(unittest.TestCase):
 
     def setUp(self):
         self.correct_reference = Reference(
-            "Test-driven Python development : develop high-quality and maintainable Python applications using the principles of test-driven development ", 
+            "Test-driven Python development : develop high-quality and maintainable Python applications using the principles of test-driven development", 
             2015, 
             "Siddharta Govindaraj"
         )
+
+    def test_create_reference(self):
+        self.assertEqual(self.correct_reference.title, "Test-driven Python development : develop high-quality and maintainable Python applications using the principles of test-driven development")
+        self.assertEqual(self.correct_reference.year, 2015)
+        self.assertEqual(self.correct_reference.authors, "Siddharta Govindaraj")
