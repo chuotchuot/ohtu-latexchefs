3a9975fdd0430fd222b1a430677dfe9c99b7b8ce
Juho Kavekari <juhokavekari@hotmail.com>
2024-11-20 17:34:52 +0200

    Update reference class to include reference key and keywords

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,8 +1,10 @@
 class Reference:
-    def __init__(self, title, year, authors):
+    def __init__(self, title, year, authors, reference_key, keywords):
         self.title = title
         self.year = year
         self.authors = authors
+        self.reference_key = reference_key
+        self.keywords = keywords
 
     def __str__(self):
         pass
