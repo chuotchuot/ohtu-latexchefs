1f81599e0dcf40ba81d562ae4b32b1fd72b2ae47
Juho Kavekari <juhokavekari@hotmail.com>
2024-11-20 17:52:59 +0200

    Add reference key and keywords to unit tests

--- src/unit_tests/reference_test.py
+++ src/unit_tests/reference_test.py
@@ -7,10 +7,14 @@ class TestReference(unittest.TestCase):
         self.correct_reference = Reference(
             "Test-driven Python development : develop high-quality and maintainable Python applications using the principles of test-driven development", 
             2015, 
-            "Siddharta Govindaraj"
+            "Siddharta Govindaraj",
+            "TDDPython",
+            "Programming, Agile, Testing, Software development"
         )
 
     def test_create_reference(self):
         self.assertEqual(self.correct_reference.title, "Test-driven Python development : develop high-quality and maintainable Python applications using the principles of test-driven development")
         self.assertEqual(self.correct_reference.year, 2015)
         self.assertEqual(self.correct_reference.authors, "Siddharta Govindaraj")
+        self.assertEqual(self.correct_reference.reference_key, "TDDPython")
+        self.assertEqual(self.correct_reference.keywords, "Programming, Agile, Testing, Software development")
