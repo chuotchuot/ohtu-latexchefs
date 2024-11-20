6647b0d009b8c2cfd836cac88421f28bbd083fc4
Juho Kavekari <juhokavekari@hotmail.com>
2024-11-20 17:31:57 +0200

    Add columns reference_key and keywords to reference table

--- src/db_helper.py
+++ src/db_helper.py
@@ -17,7 +17,6 @@ def table_exists(name):
 
     result = db.session.execute(sql)
     return result.fetchall()[0][0]
-    
 
 def setup_db():
     if table_exists(table_name):
@@ -35,18 +34,19 @@ def setup_db():
         "   year INT," 
         "   publisher TEXT," 
         "   editor TEXT,"
-        "   reference_type TEXT)"
+        "   reference_type TEXT,"
+        "   reference_key TEXT"
+        "   keywords TEXT"
+        ")"
         ))
     db.session.execute(sql)
     db.session.commit()
-
 
 def reset_db():
     print(f"Clearing contents from table {table_name}")
     sql = text(f"DELETE FROM {table_name}")
     db.session.execute(sql)
     db.session.commit()
-
 
 if __name__ == "__main__":
     with app.app_context():
