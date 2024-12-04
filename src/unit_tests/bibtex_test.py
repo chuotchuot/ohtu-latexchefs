7abdcea4ff430e4b1ca110548caa367a0cf73133
Juho Kavekari <juhokavekari@hotmail.com>
2024-12-04 13:57:07 +0200

    Add more unit tests

--- src/unit_tests/bibtex_test.py
+++ src/unit_tests/bibtex_test.py
@@ -6,6 +6,27 @@ class TestReferenceInput(unittest.TestCase):
 
     def setUp(self):
 
+        self.test_book_dict = {
+            "ENTRYTYPE": "book",
+            "ID": "PyUnitTest-17",
+            "title": "Python Unit Test Automation",
+            "author": "Ashwin Pajankar",
+            "year": "2017",
+            "publisher": "Berkeley, CA : Apress : Imprint: Apress",
+            "editor": "Joku random"
+        }
+
+        self.test_author_and_keyword_dict = {
+            "ENTRYTYPE": "book",
+            "ID": "PyUnitTest-17",
+            "title": "Python Unit Test Automation",
+            "author": "Ashwin Pajankar and Pashwin Ajankar",
+            "year": "2017",
+            "publisher": "Berkeley, CA : Apress : Imprint: Apress",
+            "editor": "Joku random",
+            "keywords": "Testing, Automation"
+        }
+
         self.test_book = Reference(
             "Python Unit Test Automation",
             "book",
@@ -29,6 +50,26 @@ class TestReferenceInput(unittest.TestCase):
 
         bibtex_string = reference_repository.create_bibtex_string(self.test_book)
 
-        print(test_string)
-
         self.assertEqual(bibtex_string, test_string)
+
+    def test_creating_bibtex_instance(self):
+
+        bibtex_instance = reference_repository.create_bibtex_instance(self.test_book)
+
+        self.assertDictEqual(bibtex_instance, self.test_book_dict)
+
+    def test_formatting_multiple_authors(self):
+
+        correct_format_authors = "Ashwin Pajankar and \n          Pashwin Ajankar"
+
+        bibtex = reference_repository.bibtex_seperate_multiple(self.test_author_and_keyword_dict)
+
+        self.assertEqual(bibtex["author"], correct_format_authors)
+
+    def test_formatting_multiple_keywords(self):
+
+        correct_format_keywords = "Testing,\n             Automation"
+
+        bibtex = reference_repository.bibtex_seperate_multiple(self.test_author_and_keyword_dict)
+
+        self.assertEqual(bibtex["keywords"], correct_format_keywords)
--- src/unit_tests/reference_test.py
+++ src/unit_tests/reference_test.py
@@ -1,6 +1,33 @@
 import unittest
+from entities.reference import Reference
+from repositories import reference_repository
 
 class TestReference(unittest.TestCase):
 
     def setUp(self):
-        pass
+
+        self.test_book = Reference(
+            "Testikirja",
+            "book",
+            "PyUnitTest-24",
+            "2024",
+            "Jukka Heikkinen",
+            "Otava",
+            "Marko Mäkinen"
+        )
+
+    def test_creating_empty_reference_dictionary(self):
+
+        test_dict = reference_repository.create_input_dictionary()
+
+        for key, value in test_dict.items():
+            self.assertEqual(value, "")
+            self.assertIsInstance(key, str)
+
+    def test_creating_readable_string(self):
+
+        correct_string = "Testikirja, Jukka Heikkinen, 2024, Otava, Marko Mäkinen"
+
+        readable_string = reference_repository.create_readable_string(self.test_book)
+
+        self.assertEqual(correct_string, readable_string)
