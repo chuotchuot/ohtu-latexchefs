1c6cae8ad34a6d389d0d08c5b6303ecb3349524c
Leevi <114101377+leevitukia@users.noreply.github.com>
2024-12-11 14:09:40 +0200

    Created demo_setup.py for automatically setting up the references for the demo

---
+++ src/demo_setup.py
@@ -0,0 +1,18 @@
+from db_helper import setup_db
+from config import app
+from entities.reference import Reference
+from repositories.reference_repository import save_reference, generate_reference_key
+
+if __name__ == "__main__":
+    with app.app_context():
+        setup_db()
+        reference1: Reference = Reference()
+        reference1.title = "Test Title"
+        reference1.authors = "Author"
+        reference1.year = "2024"
+        reference1.reference_type = "book"
+        reference1.publisher = "Publisher"
+        reference1.reference_key = generate_reference_key(reference1)
+        save_reference(reference1)
+
+    
