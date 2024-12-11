c465d302662cc7ac53af80ea71865010d851adcb
Nuutti Kekkonen <nuutti.kekkonen@saunalahti.fi>
2024-12-11 16:12:42 +0200

    Removed duplicate function

--- tasks.py
+++ tasks.py
@@ -9,3 +9,8 @@ def test(ctx):
     ctx.run("pytest src/unit_tests", pty=True)
     ctx.run("robot src/story_tests", pty=True)
     ctx.run("pylint src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage html", pty=True)
