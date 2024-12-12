e325be7e237cb195d0420c4851e99b620161e290
chuot <152192561+chuotchuot@users.noreply.github.com>
2024-12-12 21:23:26 +0200

    Create additional unit tests for reference.py

--- src/unit_tests/reference_test.py
+++ src/unit_tests/reference_test.py
@@ -68,3 +68,59 @@ class TestReference(unittest.TestCase):
         self.test_input_book.format_fields_with_multiple_values()
 
         self.assertEqual(correct_str, self.test_input_book.keywords)
+
+    def test_add_values_from_dictionary(self):
+        input_book_dict = {
+            "title": "Java Unit Testing with JUnit 5 : Test Driven Development with JUnit 5",
+            "reference_type": "book",
+            "reference_key": "JavaUT-17",
+            "year": "2017",
+            "authors": "Shekhar Gulati;Rahul Sharma",
+            "publisher": "Berkeley, CA : Apress : Imprint: Apress",
+            "editors": "Shekhar Gulati;Rahul Sharma",
+            "keywords": "Testing;Java;Development"
+        }
+
+        reference = Reference()
+        reference.add_values_from_dictionary(input_book_dict.items())
+
+        self.assertEqual(reference.title, self.test_input_book.title)
+        self.assertEqual(reference.reference_type, self.test_input_book.reference_type)
+        self.assertEqual(reference.reference_key, self.test_input_book.reference_key)
+        self.assertEqual(reference.year, self.test_input_book.year)
+        self.assertEqual(reference.authors, self.test_input_book.authors)
+        self.assertEqual(reference.publisher, self.test_input_book.publisher)
+        self.assertEqual(reference.editors, self.test_input_book.editors)
+        self.assertEqual(reference.keywords, self.test_input_book.keywords)
+
+    def test_add_values_from_doi(self):
+        data = {
+            "title": "Software unit test coverage and adequacy",
+            "author": "Zhu, Hong and Hall, Patrick A. V. and May, John H. R.",
+            "year": "1997",
+            "journal": "ACM Comput. Surv.",
+            "volume": "29",
+            "number": "4",
+            "page": "366–427",
+            "month": "dec",
+        }
+
+        reference = Reference()
+        reference.add_values_from_doi(data)
+
+        self.assertEqual(reference.title, data.get("title"))
+        self.assertEqual(reference.authors, data.get("author"))
+        self.assertEqual(reference.year, data.get("year"))
+        self.assertEqual(reference.journal, data.get("journal"))
+        self.assertEqual(reference.volume, data.get("volume"))
+        self.assertEqual(reference.number, data.get("number"))
+        self.assertEqual(reference.page, data.get("page"))
+        self.assertEqual(reference.month, data.get("month"))
+
+    def test_set_reference_key(self):
+        test_ref_key = "RefKey"
+
+        reference = Reference()
+        reference.set_reference_key(test_ref_key)
+
+        self.assertEqual(test_ref_key, reference.reference_key)
